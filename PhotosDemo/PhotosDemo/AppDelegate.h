eea457561c09e470e4efa2f75000491f3208a788
zhangkang <zhangkangjiqi@163.com>
2017-10-17 12:29:01 +0800

    upload Progect

---
+++ PhotosDemo/PhotosDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotosDemo
+//
+//  Created by LSH on 2017/10/17.
+//  Copyright © 2017年 ZhangKang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotosDemo/PhotosDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhotosDemo
+//
+//  Created by LSH on 2017/10/17.
+//  Copyright © 2017年 ZhangKang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
